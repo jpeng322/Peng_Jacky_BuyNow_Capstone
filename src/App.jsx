987b2befed14b05a79609dc313a35608505c1a06
Jacky <jackypengsmail@gmail.com>
2024-01-22 00:44:53 -0500

    Create pages using react router

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/shop",
+    element: <Shop />,
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;

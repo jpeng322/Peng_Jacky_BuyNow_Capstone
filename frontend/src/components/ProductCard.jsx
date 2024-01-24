cb953c9051cd3fdc458456e66503db05f997f6fa
Jacky <jackypengsmail@gmail.com>
2024-01-23 21:56:46 -0500

    Create shop page

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -45,34 +45,47 @@ import products from "../../data2";
 //   );
 // };
 
-const ProductCard = () => {
-  const firstItem = products.data[0];
+const ProductCard = ({ product }) => {
+  if (!product) return;
   return (
-    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-5 flex flex-col gap-3">
+    <div className="border  bg-white border border-gray-200 rounded-lg shadow  p-5 flex flex-col gap-3">
       <div>
-        <h2 className="text-lg font-bold text-neutral-900">
+        <h2 className="text-lg font-bold text-neutral-900 line-clamp-2   ">
           {" "}
-          {firstItem.product_title}
+          {product.product_title}
         </h2>
-        <p className="text-sm text-gray-400 font-semibold mt-1">
-          Estimated Price: {firstItem.offer.price}
-        </p>
+        <div className="flex justify-between items-center">
+          <p className="text-sm text-gray-400 font-semibold mt-1 ">
+            Estimated Price: {product.offer.price}
+          </p>
+
+          <div className="flex font-bold items-center gap-1">
+            <p>{product.offer.store_rating} </p>
+            <StarIcon fill="red" />
+          </div>
+        </div>
       </div>
       <div>
         <img
           alt="Product Image"
           className="rounded-lg object-cover w-full aspect-square"
           height={200}
-          src={firstItem.product_photos[0]}
+          src={product.product_photos[0]}
           width={200}
         />
       </div>
-      <footer className="flex flex-col gap-2 mt-4">
+      <footer className="flex flex-col gap-2 mt-4 border-red-400">
         <a
           href="#"
           className="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-green-600 hover:bg-green-700  "
         >
-          Add to cart
+          Add to Cart
+        </a>
+        <a
+          href="#"
+          className="border border-green-600 text-green-600 border-2 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:text-green-700 "
+        >
+          More Information
         </a>
       </footer>
     </div>
@@ -82,18 +95,13 @@ const ProductCard = () => {
 function StarIcon(props) {
   return (
     <svg
-      {...props}
+      className="w-4 h-4 text-yellow-300"
+      aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
+      viewBox="0 0 22 20"
     >
-      <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+      <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
     </svg>
   );
 }
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -4,10 +4,17 @@ import ProductCard from "../components/ProductCard";
 
 const Shop = () => {
   console.log(products);
+  const listOfProducts = products.data;
   return (
     <div>
       <h1>This is the Shop page</h1>
-      <ProductCard />
+      <div className="grid grid-cols-5 gap-5 border border-blue-200 p-[3vw]">
+        {listOfProducts.map((product) => {
+          console.log(product);
+          return <ProductCard key={product.product_id} product={product} />;
+        })}
+      </div>
+      {/* <ProductCard /> */}
     </div>
   );
 };
